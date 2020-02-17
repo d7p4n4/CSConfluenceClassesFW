96363b0b16a3a670bf24b7dd7091537b51f2f90b
Zsombor <zsombor.berces@artanacsado.hu>
2020-02-17 16:26:24 +0100

    1.20200217.1

--- CSConfluenceClassesFW/AddNewPage/AddNewPageSuccessResponse.cs
+++ CSConfluenceClassesFW/AddNewPage/AddNewPageSuccessResponse.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
-namespace CSConfluenceClassesFW
+namespace AddNewPage
 {
     public class AddNewPageSuccessRessponse
     {
