8c2bcf6edf6bfc7f285815fa9df5f7a5df11ee8a
Zsombor <zsombor.berces@artanacsado.hu>
2020-02-18 09:43:55 +0100

    1.20200218.3

---
+++ CSConfluenceClassesFW/DeletePage/DeletePageSuccessResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CSConfluenceClassesFW.DeletePage
+{
+    public class DeletePageSuccessResponse
+    {
+    }
+}
